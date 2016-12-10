037c4a55709c84004295fe4e3f21a0bd8dd81dad
alpipego <alpipego@gmail.com>
2016-12-10 09:35:14 +0100

    make string translateable, added meta info

--- version-info.php
+++ version-info.php
@@ -1,18 +1,29 @@
 <?php
-/*
-Plugin Name: WordPress and Environment Version Number in Footer
-Plugin URI: https://wordpress.org/plugins/version-info
-Description: Show current WordPress, PHP, Web Server and MySQL versions in admin footer
-Author: alpipego
-Version: 1.0.2
-Author URI: http://alpipego.com/
-*/
-
+/**
+ * WordPress Version Info
+ *
+ * @package     wp-version-info
+ * @author      Alexander Goller <alpipego@gmail.com>
+ * @copyright   2016 Alexander Goller
+ * @license     MIT
+ *
+ * @wordpress-plugin
+ *
+ * Plugin Name: WordPress Version Info
+ * Plugin URI: https://wordpress.org/plugins/version-info
+ * Description: Show current WordPress, PHP, Web Server and MySQL versions in admin footer
+ * Author: alpipego
+ * Author URI: http://alpipego.com/
+ * Version: 1.0.2
+ * License: MIT
+ * GitHub Plugin URI: https://github.com/alpipego/wp-version-info
+ * Text Domain: version-info
+ */
 // pseudo namespace
 class VersionInfo {
 
 	public function __construct() {
-		add_filter( 'update_footer', array($this, 'version_in_footer'), 11 );
+		add_filter( 'update_footer', array( $this, 'version_in_footer' ), 11 );
 	}
 
 	public function version_in_footer() {
@@ -23,7 +34,7 @@ class VersionInfo {
 		$mysql_server = explode( '-', mysqli_get_server_info( $mysqli ) );
 		$mysqli->close();
 
-		return sprintf( 'You are running WordPress %s  | PHP %s | %s | MySQL %s', $wp_version, phpversion(), $_SERVER['SERVER_SOFTWARE'], $mysql_server[0] );
+		return sprintf( esc_attr__('You are running WordPress %s  | PHP %s | %s | MySQL %s', 'version-info'), $wp_version, phpversion(), $_SERVER['SERVER_SOFTWARE'], $mysql_server[0] );
 	}
 }
 
